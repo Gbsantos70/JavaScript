677c680d46375420c8eef3c1de70b8baba7c415d
Gabriel Santos <gabriel.s.souza8@aluno.senai.br>
2023-12-13 21:26:29 -0300

    estilizacao

--- games-api/js/main.js
+++ games-api/js/main.js
@@ -5,12 +5,25 @@ window.onload = () => {
 };
 
 const loadGames = () => {
-  console.log(">>>");
   const dataContainer = document.getElementById("data-container");
   getAllGames().then((resp) => {
-    resp.forEach((game) => {
+    resp.forEach((jogo) => {
       const gamesElement = document.createElement("div");
-      gamesElement.innerHTML = `<strong>${game.nome}</strong><p>${game.preco}</p>`;
+      gamesElement.innerHTML = `
+     
+      <div class="elemento">
+      <img class="cardimg" src="${jogo.img}" alt="${jogo.nome}">
+    
+      <hr>
+     
+    <h5 class="cardtitle">${jogo.nome}</h5>
+    
+    <hr>
+  
+    <p class="cardtext"> R$ ${jogo.preco}</p>       
+      </div>
+      `;
+
       dataContainer.appendChild(gamesElement);
     });
   });
